d5f2f92ed2106fda60e622a3ebe16e42d2f821ac
suparansharma <suparansharmap@gmail.com>
2024-08-02 21:48:40 +0600

    update new template

---
+++ pages/expense/index.jsx
@@ -0,0 +1,225 @@
+import React, { useEffect, useState, useCallback } from "react";
+import { Row, Table, Tag } from 'antd';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import DebouncedSearchInput from '@/components/elements/DebouncedSearchInput';
+import { DeleteOutlined, EditOutlined, EyeOutlined } from '@ant-design/icons';
+import withAuth from '@/components/withAuth';
+import Axios from '@/utils/axios';
+import { EXPENSE_END_POINT } from "@/constants/api_endpoints/expenseEndPoints";
+import ToastMessage from "@/components/Toast";
+
+
+const Expense = () => {
+    /*** Storing data start */
+    const { http } = Axios();
+    const [expenseList, setExpenseList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
+    const [perPage, setPerPage] = useState(10);
+    const [limit, setLimit] = useState(10);
+    const [editData, setEditData] = useState({});
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isViewModalOpen, setViewIsModalOpen] = useState(false);
+    const [isDeleteModalOpen, setDeleteIsModalOpen] = useState(false);
+    const [search, setSearch] = useState('');
+    
+    /*** Storing data end */
+    
+    console.log('expenseList ',expenseList)
+    /**Add function  start */
+    const handleAdd = () => {
+        setIsModalOpen(true);
+        setEditData(null);
+    };
+    /**Add function end */
+    
+    
+    /** edit function start */
+    const handleEdit = (data) => {
+        setEditData(data);
+        setIsModalOpen(true);
+    };
+    /** edit function  end */
+    
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+    
+    
+    
+    /** Delete function start */
+    const handleDelete = (data) => {
+        console.log("clcik")
+        setEditData(data);
+        setDeleteIsModalOpen(true);
+    };
+    const closeDeleteModal = () => {
+        setDeleteIsModalOpen(false);
+    };
+    /** Delete function end */
+    
+    
+    
+    /**Render Function Start */
+    const reFetchHandler = (isRender) => {
+        if (isRender) fetchExpenseList();
+    };
+    /**Render Function end */
+    
+    
+    /***Fetching table Data Start */
+    
+    const fetchExpenseList = async () => {
+        try {
+            const response = await http.get(EXPENSE_END_POINT.list());
+            setExpenseList(response.data?.data);
+            setLoading(false);
+        } catch (error) {
+            console.error('Error fetching seller list:', error);
+            setLoading(false);
+        }
+    };
+    
+    useEffect(() => {
+        fetchExpenseList();
+        return () => {
+        };
+    }, []);
+    
+    /***Fetching table Data end */
+    
+    
+    
+    
+    
+    
+    const columns = [
+        {
+            title: 'SL',
+            fixed: 'left',
+            render: (text, record, index) => index + 1
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            // fixed: 'left',
+        },
+        
+        {
+            title: 'Date',
+            dataIndex: 'date',
+            // fixed: 'left',
+        },
+        {
+            title: 'Amount',
+            dataIndex: 'amount',
+            // fixed: 'left',
+        },
+        {
+            title: 'Created At',
+            dataIndex: 'created_at',
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            fixed: 'right',
+            width: 100,
+            render: (row) => actionButton(row), // You need to define actionButton function
+        },
+    ];
+    
+    
+    
+    const actionButton = (row) => {
+        return (
+            <>
+                <Row justify="space-between" style={{ display: 'flex', alignItems: 'center' }}>
+                    <a onClick={() => handleViewOpen(row)} style={{ color: 'green' }}>
+                        <EyeOutlined style={{ fontSize: '22px' }} />
+                    </a>
+    
+                    <a onClick={() => handleEdit(row)} className="text-primary" >
+                        <EditOutlined style={{ fontSize: '22px' }} />
+                    </a>
+    
+                    <a onClick={() => handleDelete(row)} className="text-danger" >
+                        <DeleteOutlined style={{ fontSize: '22px' }} />
+                    </a>
+                </Row>
+            </>
+        );
+    };
+    
+        /*** Pagination Start  */
+        const pagination = {
+            total: expenseList?.length,
+            current: page,
+            pageSize: perPage,
+            defaultPageSize: 10,
+            showSizeChanger: true,
+            pageSizeOptions: ['2', '5', '10', '20', '30']
+        };
+    
+        const onChange = (
+            pagination,
+            filters,
+            sorter,
+            extra
+        ) => {
+            setPage(pagination.current);
+            setLimit(pagination.pageSize);
+        };
+    
+        /*** Pagination End  */
+    
+        const toggleModal = () => {
+            console.log("clicked");
+            setIsModalOpen(!isModalOpen);
+        };
+    
+        return (
+            <div className="flex flex-col gap-10">
+                <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+                    <div className="py-6 px-4 md:px-6 xl:px-7.5 flex justify-between items-center border-b border-stroke dark:border-strokedark">
+                        <h4 className="text-xl font-semibold text-black dark:text-white">
+                            All Expense
+                        </h4>
+                        <button
+                            href="#"
+                            className="inline-flex items-center justify-center rounded-full bg-primary py-2 px-5 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10"
+                            onClick={handleAdd}
+                        >
+                            Add
+                            <span className="button-icon-space ml-5">
+                                <FontAwesomeIcon icon={faPlusCircle} />
+                            </span>
+                        </button>
+                    </div>
+    
+    
+    
+                    <div className="p-4 md:p-6 xl:p-7.5 flex justify-end">
+                        <DebouncedSearchInput setSearch={setSearch} />
+                    </div>
+                    {/* <DeleteModal isOpen={isDeleteModalOpen} onClose={closeDeleteModal} data={editData} isParentRender={reFetchHandler} />
+                    <ExpensecategoryForm isOpen={isModalOpen} onClose={closeModal} setEditData={editData} isParentRender={reFetchHandler} />
+    */}
+                    <Table
+                        className="border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark text-black dark:text-white"
+                        columns={columns}
+                        dataSource={expenseList}
+                        scroll={{ x: 'max-content' }}
+                        pagination={pagination}
+                        onChange={onChange}
+                    /> 
+    
+                   
+                </div>
+            </div>
+    
+    
+        )
+    }
+    
+    export default withAuth(Expense)
