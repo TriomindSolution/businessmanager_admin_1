30091f3772b5e7124c59ff67378bb94e1f00d179
suparansharma <suparansharmap@gmail.com>
2024-11-02 00:26:22 +0600

    update all

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,9 @@
-
 import { SECURITY_END_POINT } from "./api_endpoints/securityEndPoints";
-
-
-export {  SECURITY_END_POINT};
-
+import { SELLER_END_POINT } from "./api_endpoints/sellerEndPoints";
+import { EXPENSECATEGORY_END_POINT } from "./api_endpoints/expensecategoryEndPoints";
+import { EXPENSE_END_POINT } from "./api_endpoints/expenseEndPoints";
+import { CUSTOMER_END_POINT } from "./api_endpoints/customerEndPoints";
+import { CATEGORY_END_POINT } from "./api_endpoints/categoryEndPoints";
+import { PRODUCT_END_POINT } from "./api_endpoints/productEndPoints";
+import { ORDER_END_POINT } from "./api_endpoints/orderEndPoints";
+export { ORDER_END_POINT, SECURITY_END_POINT, SELLER_END_POINT, EXPENSECATEGORY_END_POINT, EXPENSE_END_POINT, CATEGORY_END_POINT, PRODUCT_END_POINT };
